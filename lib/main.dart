e956521452b1bdb6a668cfa08d2e483ef0187883
vohoangloc123 <you@example.com>
2024-06-28 20:23:24 +0700

    Game Logic + AlertDialog

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -23,41 +24,99 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> displayExOh=['','','','','','','','',''];
+  bool ohTurn = true;
+  List<String> displayExOh = ['', '', '', '', '', '', '', '', ''];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
       body: GridView.builder(
           itemCount: 9,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3),
-          itemBuilder: (BuildContext content, int index){
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (BuildContext content, int index) {
             return GestureDetector(
-              onTap: (){
+              onTap: () {
                 _tapped(index);
               },
               child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey[700]!)
-                ),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey[700]!)),
                 child: Center(
-                  child: Text(displayExOh[index],
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40),),
+                  child: Text(
+                    displayExOh[index],
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
                 ),
               ),
             );
           }),
     );
   }
-  void _tapped(int index){
+
+  void _tapped(int index) {
     setState(() {
-      displayExOh[index]='o';
+      if (ohTurn) {
+        displayExOh[index] = 'o';
+      } else {
+        displayExOh[index] = 'x';
+      }
+      ohTurn = !ohTurn;
+      _checkWinner();
     });
   }
+
+  void _checkWinner() {
+    // Kiểm tra hàng ngang
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    } else if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != '') {
+      _showWinDialog(displayExOh[3]);
+    } else if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != '') {
+      _showWinDialog(displayExOh[6]);
+    }
+
+    // Kiểm tra hàng dọc
+    else if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    } else if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != '') {
+      _showWinDialog(displayExOh[1]);
+    } else if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != '') {
+      _showWinDialog(displayExOh[2]);
+    }
+
+    // Kiểm tra đường chéo
+    else if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    } else if (displayExOh[2] == displayExOh[4] &&
+        displayExOh[2] == displayExOh[6] &&
+        displayExOh[2] != '') {
+      _showWinDialog(displayExOh[2]);
+    }
+  }
+
+  void _showWinDialog(String winner) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Winner: '+winner),
+        );
+      },
+    );
+  }
 }
-
-
-
