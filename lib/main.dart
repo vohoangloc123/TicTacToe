af76106b2c1048d27795f2a69e01b10d09267171
vohoangloc123 <you@example.com>
2024-06-29 12:08:07 +0700

    added scoreboard

--- lib/main.dart
+++ lib/main.dart
@@ -26,39 +26,83 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool ohTurn = true;
   List<String> displayExOh = ['', '', '', '', '', '', '', '', ''];
+  var myTextStyle = TextStyle(color: Colors.white, fontSize: 30);
+  int ohScore = 0;
+  int exScore = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
-      body: GridView.builder(
-          itemCount: 9,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-          itemBuilder: (BuildContext content, int index) {
-            return GestureDetector(
-              onTap: () {
-                _tapped(index);
-              },
-              child: Container(
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.grey[700]!)),
-                child: Center(
-                  child: Text(
-                    displayExOh[index],
-                    style: TextStyle(color: Colors.white, fontSize: 40),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Player o', style: myTextStyle),
+                      Text(ohScore.toString(), style: myTextStyle),
+                    ],
                   ),
                 ),
-              ),
-            );
-          }),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Player x', style: myTextStyle),
+                      Text(exScore.toString(), style: myTextStyle),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+              itemCount: 9,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+              itemBuilder: (BuildContext content, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    _tapped(index);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey[700]!)),
+                    child: Center(
+                      child: Text(
+                        displayExOh[index],
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.grey[700],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   void _tapped(int index) {
     setState(() {
-      if (ohTurn) {
+      if (ohTurn && displayExOh[index] == '') {
         displayExOh[index] = 'o';
-      } else {
+      } else if (!ohTurn && displayExOh[index] == '') {
         displayExOh[index] = 'x';
       }
       ohTurn = !ohTurn;
@@ -111,12 +155,37 @@ class _HomePageState extends State<HomePage> {
 
   void _showWinDialog(String winner) {
     showDialog(
+      barrierDismissible:
+          false, //Để đảm bảo hộp thoại không thể bị loại bỏ bằng cách chạm vào bên ngoài hộp thoại
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Winner: '+winner),
+          title: Text('Winner: ' + winner),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Play Again!'),
+              onPressed: () {
+                _clearBoard();
+                Navigator.of(context).pop();
+              },
+            )
+          ],
         );
       },
     );
+    if (winner == 'o') {
+      ohScore += 1;
+    } else if (winner == 'x') {
+      exScore += 1;
+    }
+    _clearBoard();
+  }
+
+  void _clearBoard() {
+    setState(() {
+      for (int i = 0; i < 9; i++) {
+        displayExOh[i] = '';
+      }
+    });
   }
 }
